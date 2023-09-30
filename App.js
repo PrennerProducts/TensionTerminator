326835bcb8d7de26c5d17c429804360bd9d6ffaa
Zetamasevt <92745665+Zetamasevt@users.noreply.github.com>
2023-09-30 19:27:58 +0200

    some improvements
    buttons for start and exit evaluation, detect left and right, calculate max yaw right and left, draw line between eyes,

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { Camera } from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
@@ -9,8 +9,12 @@ export default function App() {
   const [yaw, setYaw] = useState(0);
   const [roll, setRoll] = useState(0);
   const [Neigung, setNeigung] = useState(0);
-  const [maxYaw, setMaxYaw] = useState(0);
+  const [ScreenText, setScreenText] = useState("");
+  const [maxYawR, setMaxYawR] = useState(0);
+  const [maxYawL, setMaxYawL] = useState(0);
   const [maxRoll, setMaxRoll] = useState(0);
+  const [evaluationStarted, setEvaluationStarted] = useState(false);
+  const [lineCoordinates, setLineCoordinates] = useState(null); // Store line coordinates
 
   useEffect(() => {
     (async () => {
@@ -23,33 +27,14 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
 
-  function calculateFaceBox(face) {
-    // Calculate face box position and dimensions.
-    const { bounds } = face;
-    const left = bounds.origin.x;
-    const top = bounds.origin.y;
-    const width = bounds.size.width;
-    const height = bounds.size.height;
-
-    return {
-      left,
-      top,
-      width,
-      height,
-    };
-  }
-
   const handleFacesDetected = ({ faces }) => {
-    // Process face data and update yaw and roll angles here
-    if (faces.length > 0) {
-      const face = faces[0]; // Assuming there's only one detected face
-      const faceBox = calculateFaceBox(face);
+    if (faces.length === 0) return;
+    else if (faces.length > 0) {
+      const face = faces[0];
       let n = 0;
-
-      // Update yaw and roll angles.
       setYaw(face.yawAngle.toFixed(2));
       setRoll(face.rollAngle.toFixed(2));
-      setMaxYaw((prev) => Math.max(prev, Math.abs(face.yawAngle)));
+
       setMaxRoll((prev) => Math.max(prev, Math.abs(face.rollAngle)));
       n = face.yawAngle.toFixed(2);
       if (n > 80) {
@@ -57,75 +42,158 @@ export default function App() {
         n = n.toFixed(2);
       }
       setNeigung(n);
-    }
+      let rotoDegree = 0;
+      rotoDegree = face.yawAngle.toFixed(0);
+      if (rotoDegree > 15 && rotoDegree < 180) {
+        setScreenText("-> " + rotoDegree + "°");
+        if (rotoDegree > maxYawR) setMaxYawR(rotoDegree);
+      }
+      else if (rotoDegree < 345 && rotoDegree > 180) {
+        setScreenText("<- " + (360-rotoDegree) + "°");
+        if ((360-rotoDegree) > maxYawL) setMaxYawL(360-rotoDegree);
+      }
+      else setScreenText("");
+      //console.log(ScreenText);
 
-    setFaceData(faces);
+      // Calculate line coordinates between left eye and right eye
+      if (face.LEFT_EYE && face.RIGHT_EYE) {
+        const leftEye = face.RIGHT_EYE;
+        const rightEye = face.LEFT_EYE;
+        const lineCoordinates = {
+          startX: leftEye.x,
+          startY: leftEye.y,
+          endX: rightEye.x,
+          endY: rightEye.y,
+        };
+        setLineCoordinates(lineCoordinates);
+      }
+    } else {
+      setLineCoordinates(null); // Reset line coordinates
+    }
+  };
+
+  const startEvaluation = () => {
+    setEvaluationStarted(true);
+  };
+
+  const exitEvaluation = () => {
+    setEvaluationStarted(false);
+    setMaxYawR(0); // Reset max yaw to 0
+    setMaxYawL(0); // Reset max yaw to 0
+    setMaxRoll(0); // Reset max roll to 0
+    setFaceData([]); // Clear face data
+    setLineCoordinates(null); // Reset line coordinates
   };
 
   return (
-    <Camera
-      type={Camera.Constants.Type.front}
-      style={styles.camera}
-      onFacesDetected={handleFacesDetected}
-      faceDetectorSettings={{
-        mode: FaceDetector.FaceDetectorMode.accurate,
-        detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
-        runClassifications: FaceDetector.FaceDetectorClassifications.none,
-        minDetectionInterval: 100,
-        tracking: true,
-      }}
-    >
-      {faceData.map((face, index) => {
-        const faceBox = calculateFaceBox(face);
-
-        return (
-          <React.Fragment key={index}>
-            {/* Draw a transparent face box */}
+    <View style={styles.container}>
+      {evaluationStarted ? (
+        <Camera
+          type={Camera.Constants.Type.front}
+          style={styles.camera}
+          onFacesDetected={handleFacesDetected}
+          faceDetectorSettings={{
+            mode: FaceDetector.FaceDetectorMode.fast,
+            detectLandmarks: FaceDetector.FaceDetectorLandmarks.all,
+            runClassifications: FaceDetector.FaceDetectorClassifications.none,
+            minDetectionInterval: 0,
+            tracking: true,
+          }}
+        >
+          {/* Draw the line when lineCoordinates is available */}
+          {lineCoordinates && (
             <View
               style={{
                 position: 'absolute',
-                left: faceBox.left,
-                top: faceBox.top,
-                width: faceBox.width,
-                height: faceBox.height,
-                borderColor: 'green',
-                borderWidth: 2,
-                borderRadius: 10,
-                opacity: 0.7,
+                left: lineCoordinates.startX,
+                top: lineCoordinates.startY,
+                width: Math.sqrt(
+                  Math.pow(lineCoordinates.endX - lineCoordinates.startX, 2) +
+                    Math.pow(lineCoordinates.endY - lineCoordinates.startY, 2)
+                ),
+                height: 2,
+                backgroundColor: 'red',
+                transform: [
+                  {
+                    rotate: `${Math.atan2(
+                      lineCoordinates.endY - lineCoordinates.startY,
+                      lineCoordinates.endX - lineCoordinates.startX
+                    )}rad`,
+                  },
+                ],
+                transformOrigin: '0% 0%',
               }}
-            ></View>
-            {/* Display yaw and roll angles */}
-            <Text style={styles.faceDesc}>
-              Yaw: {yaw}°, Roll: {roll}°, Neigung: {Neigung}°
-            </Text>
-            <Text style={styles.maxValues}>
-              Max Yaw: {maxYaw.toFixed(2)}°, Max Roll: {maxRoll.toFixed(2)}°
-            </Text>
-          </React.Fragment>
-        );
-      })}
-    </Camera>
+            ></View>            
+          )}
+        </Camera>
+      ) : (
+        <View style={styles.startScreen}>
+          <TouchableOpacity onPress={startEvaluation}>
+            <Text style={styles.startButton}>Start evaluation</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {evaluationStarted && (
+        <TouchableOpacity onPress={exitEvaluation} style={styles.exitButton}>
+          <Text style={styles.exitButtonText}>Exit</Text>
+        </TouchableOpacity>
+      )}
+      {evaluationStarted && (
+        <View style={styles.textContainer}>
+          <Text style={styles.faceDesc}>
+            {ScreenText}
+          </Text>
+          <Text style={styles.maxValues}>
+            Max Right: {maxYawR.toFixed(2)}°, Max Left: {maxYawL.toFixed(2)}°
+          </Text>
+        </View>
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  camera: {
+  container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
   },
-  faceDesc: {
+  startScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  startButton: {
     fontSize: 20,
     color: 'white',
+    padding: 10,
+    backgroundColor: 'blue',
+  },
+  camera: {
+    flex: 1,
+  },
+  exitButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  exitButtonText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  textContainer: {
     position: 'absolute',
     bottom: 16,
     left: 16,
   },
+  faceDesc: {
+    fontSize: 20,
+    color: 'white',
+  },
   maxValues: {
     fontSize: 20,
     color: 'white',
-    position: 'absolute',
-    bottom: 32,
-    right: 16,
   },
 });
