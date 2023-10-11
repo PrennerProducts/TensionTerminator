4ee966265a00e871932fb0d2968dd8501ead4fee
Lukas Prenner <prenner.products@gmail.com>
2023-10-11 20:15:33 +0200

    fix warning in _layout

--- app/_layout.js
+++ app/_layout.js
@@ -28,7 +28,7 @@ export default function Layout() {
           headerShown: true,
           headerTitleAlign: 'center',
         }}
-        />
+      />
       <Stack.Screen
         name="register"
         options={{
@@ -44,126 +44,126 @@ export default function Layout() {
           ),
         }}
       />
-        <Stack.Screen
-            name="appointment"
-            options={{
-                headerTitle: 'Termin planen',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="criteria"
-            options={{
-                headerTitle: 'Ausschlusskriterien',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="dataTransfer"
-            options={{
-                headerTitle: 'Daten übermitteln',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="evaluationAfter"
-            options={{
-                headerTitle: 'Evaluierungsübung NACHHER',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="evaluationBefore"
-            options={{
-                headerTitle: 'Evaluierungsübung VORHER',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="explanationText"
-            options={{
-                headerTitle: 'Erklärung lesen',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="explanationVideo"
-            options={{
-                headerTitle: 'Erklärvideo',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="gratulation"
-            options={{
-                headerTitle: 'Gratulation',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="how"
-            options={{
-                headerTitle: 'Wie tut es weh?',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="intensityBefore"
-            options={{
-                headerTitle: 'Wie stark tut es weh? VORHER',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="intensityAfter"
-            options={{
-                headerTitle: 'Wie stark tut es weh? NACHHER',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="resultEvaluation"
-            options={{
-                headerTitle: 'Ergebnisanzeige Evaluierung',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="training"
-            options={{
-                headerTitle: 'Training',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
-        <Stack.Screen
-            name="where"
-            options={{
-                headerTitle: 'Wo tut es weh?',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
+      <Stack.Screen
+        name="appointment"
+        options={{
+          headerTitle: 'Termin planen',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
       />
-        <Stack.Screen
-            name="QRscan"
-            options={{
-                headerTitle: 'QR-Code scannen',
-                headerShown: true,
-                headerTitleAlign: 'center',
-            }}
-        />
+      <Stack.Screen
+        name="criteria"
+        options={{
+          headerTitle: 'Ausschlusskriterien',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="dataTransfer"
+        options={{
+          headerTitle: 'Daten übermitteln',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="evaluationAfter"
+        options={{
+          headerTitle: 'Evaluierungsübung NACHHER',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="evaluationBefore"
+        options={{
+          headerTitle: 'Evaluierungsübung VORHER',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="explanationText"
+        options={{
+          headerTitle: 'Erklärung lesen',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="explanationVideo"
+        options={{
+          headerTitle: 'Erklärvideo',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="gratulation"
+        options={{
+          headerTitle: 'Gratulation',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="how"
+        options={{
+          headerTitle: 'Wie tut es weh?',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="intensityBefore"
+        options={{
+          headerTitle: 'Wie stark tut es weh? VORHER',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="intensityAfter"
+        options={{
+          headerTitle: 'Wie stark tut es weh? NACHHER',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="resultEvaluation"
+        options={{
+          headerTitle: 'Ergebnisanzeige Evaluierung',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="training"
+        options={{
+          headerTitle: 'Training',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="where"
+        options={{
+          headerTitle: 'Wo tut es weh?',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="QRScan"
+        options={{
+          headerTitle: 'QR-Code scannen',
+          headerShown: true,
+          headerTitleAlign: 'center',
+        }}
+      />
 
       <Stack.Screen name="tabs" options={{ headerShown: false }} />
     </Stack>
