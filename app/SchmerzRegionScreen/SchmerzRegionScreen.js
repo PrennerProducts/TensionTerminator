754f2d194b5378b1ef0c79ae9019539a7bba0253
natariii <rene.angerer@gmail.com>
2023-10-11 15:25:54 +0200

    anpassung an Pixel 3a

--- app/SchmerzRegionScreen/SchmerzRegionScreen.js
+++ app/SchmerzRegionScreen/SchmerzRegionScreen.js
@@ -18,23 +18,23 @@ const SchmerzRegionScreen = () => {
                 {/* Schulter-Nacken */}
                 <TouchableOpacity
                     style={{
-                        backgroundColor: 'rgba(0, 0, 168, 0.2)',
+                        backgroundColor: 'rgba(0, 0, 168, 0)',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        height: windowHeight * 0.205
+                        height: windowHeight * 0.19
 
                     }}
                     onPress={() => {
                         console.log('Schulter-Nacken ausgewählt.');
                     }}
                 >
-                    <Text>Schulter-Nacken</Text>
+                    <Text style={{paddingTop:40, fontSize: 17,color: 'darkblue'}}>Schulter-Nacken</Text>
                 </TouchableOpacity>
 
                 {/* Mittlerer-Rücken */}
                 <TouchableOpacity
                     style={{
-                        backgroundColor: 'rgba(0, 0, 168, 0.2)',
+                        backgroundColor: 'rgba(0, 0, 168, 0)',
                         alignItems: 'center',
                         justifyContent: 'center',
                         height: windowHeight * 0.13,
@@ -44,39 +44,39 @@ const SchmerzRegionScreen = () => {
                         console.log('Mittlerer-Rücken ausgewählt.');
                     }}
                 >
-                    <Text>Mittlerer-Rücken</Text>
+                    <Text style={{ fontSize: 17,color: 'darkblue'}}>Mittlerer-Rücken</Text>
                 </TouchableOpacity>
 
                 {/* Unterer-Rücken */}
                 <TouchableOpacity
                     style={{
-                        backgroundColor: 'rgba(0, 0, 168, 0.2)',
+                        backgroundColor: 'rgba(0, 0, 168, 0)',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        height: windowHeight * 0.142,
+                        height: windowHeight * 0.14,
 
                     }}
                     onPress={() => {
                         console.log('Unterer-Rücken ausgewählt.');
                     }}
                 >
-                    <Text>Unterer-Rücken</Text>
+                    <Text style={{paddingBottom:20, fontSize: 17,color: 'darkblue'}}>Unterer-Rücken</Text>
                 </TouchableOpacity>
 
                 {/* Becken-Gesäß */}
                 <TouchableOpacity
                     style={{
-                        backgroundColor: 'rgba(0, 0, 168, 0.2)',
+                        backgroundColor: 'rgba(0, 0, 168, 0)',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        height: windowHeight * 0.15,
+                        height: windowHeight * 0.16,
 
                     }}
                     onPress={() => {
                         console.log('Becken-Gesäß ausgewählt.');
                     }}
                 >
-                    <Text>Becken-Gesäß</Text>
+                    <Text style={{ paddingBottom: 30, fontSize: 17,color: 'darkblue'}}>Becken-Gesäß</Text>
                 </TouchableOpacity>
             </View>
         </View>
