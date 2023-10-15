782fc67a95cbc79eb0ac3b13d2dc59d0b6f7eee4
hb02 <sascha@richter.tirol>
2023-10-15 22:53:27 +0200

    Init CriteriaScreen

--- app/criteria.jsx
+++ app/criteria.jsx
@@ -1,27 +1,131 @@
-import {View, Text, Pressable} from "react-native";
-import React from 'react';
-import { Link } from 'expo-router';
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Link } from "expo-router";
+import { getData, removeData, saveData } from "./services/storage";
+import CheckBox from "expo-checkbox";
+
+import { useIsFocused } from "@react-navigation/native";
 
 const criteriaScreen = () => {
+  const navigation = useNavigation();
+  const [firstTime, setFirstTime] = React.useState(true);
+  const isFocused = useIsFocused();
 
-    return (
+  //CheckBoxes
+  const [toggleCheckBox, setToggleCheckBox] = React.useState(false);
+
+  //Function to load our FirstTime Data. This allows the user to skip this Screen
+  const loadData = async () => {
+    const firstTime = (await getData("firstTime")) ? false : true;
+    console.log("firstTime: ", firstTime);
+    setFirstTime(firstTime);
+  };
+
+  //Use of useEffect to get our FocusHandler and to reload Data on Change of Navigation and Focus
+  React.useEffect(() => {
+    const focusHandler = navigation.addListener("focus", () => {
+      loadData();
+    });
+    return focusHandler;
+  }, [navigation, isFocused]);
+
+  const changeFirstTime = async () => {
+    await saveData("firstTime", false);
+    setFirstTime(false);
+  };
+
+  const skip = async () => {
+    removeData("firstTime");
+  };
+
+  const skipCriteria =
+    firstTime === true ? (
+      <View style={styles.buttonBottom}>
+        <Link href={"/where"} asChild>
+          <Pressable style={styles.button} onPress={skip}>
+            <Text style={styles.buttonFont}>Überspringen</Text>
+          </Pressable>
+        </Link>
+      </View>
+    ) : null;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Ausschlusskriterien</Text>
+      <Text style={styles.text}>
+        Bitte bestätigen Sie, dass Sie keine der folgenden Ausschlusskriterien
+        erfüllen:
+      </Text>
+
+      <View style={styles.section}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox}
+          onValueChange={(newValue) => setToggleCheckBox(newValue)}
+          style={styles.checkbox}
+        />
+        <Text style={styles.paragraph}>Ich bin....</Text>
+      </View>
+      <View style={styles.section}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox}
+          onValueChange={(newValue) => setToggleCheckBox(newValue)}
+          style={styles.checkbox}
+        />
+        <Text style={styles.paragraph}>Ich habe....</Text>
+      </View>
+      <View>
         <View>
-            <Text>Ausschlusskriterien</Text>
-
-            <Link href={'/where'} asChild>
-                <Pressable>
-                    <Text>Bestätigen</Text>
-                </Pressable>
-            </Link>
-            <Link href={'/where'} asChild>
-                <Pressable>
-                    <Text>Überspringen</Text>
-                </Pressable>
-            </Link>
-
+          <Link href={"/where"} asChild>
+            <Pressable onPress={changeFirstTime} style={styles.button}>
+              <Text style={styles.buttonFont}>Bestätigen</Text>
+            </Pressable>
+          </Link>
         </View>
-    );
-}
+        <View>{skipCriteria}</View>
+      </View>
+    </View>
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
+    color: "#10069F",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#10069F",
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 20,
+  },
+  buttonFont: {
+    color: "white",
+  },
+  buttonBottom: {
+    marginTop: -30,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 20,
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  checkbox: {
+    margin: 8,
+  },
+});
 
 export default criteriaScreen;
