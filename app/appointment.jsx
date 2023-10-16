a6b0834f67415adec50cb57b7545eb8656ff6552
hb02 <sascha@richter.tirol>
2023-10-16 16:02:05 +0200

    Init

--- app/appointment.jsx
+++ app/appointment.jsx
@@ -1,23 +1,90 @@
-import 'expo-router/entry';
-import { View, Text, Pressable, Button } from 'react-native';
-import React from 'react';
-import {Link, useRouter} from 'expo-router';
+import "expo-router/entry";
+import { View, Text, Pressable, Button, StyleSheet } from "react-native";
+import React from "react";
+import { Link, useRouter } from "expo-router";
 
 const Appointment = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <View>
-            <Text>Termin planen</Text>
+  setAppointment = () => {
+    console.log("Appointment set");
+  };
 
-            <Link href={'/where'} asChild>
-                <Pressable>
-                    <Text>Bestätigen</Text>
-                </Pressable>
-            </Link>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Termin planen</Text>
+      <Text
+        style={[
+          styles.text,
+          {
+            textAlign: "center",
+            fontSize: 20,
+            fontWeight: "bold",
+          },
+        ]}
+      >
+        Übung macht den Meister und bringt nachweißlich Verbesserung.
+      </Text>
+      <Text style={styles.text}>
+        Sollen wir dich an eine neue Traininssession erinnern?
+      </Text>
+      <View style={styles.section}>
+        <Text style={styles.text2}>Datum: </Text>
+        <Text style={styles.text2}>01.01.2021</Text>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.text2}>Uhrzeit: </Text>
+        <Text style={styles.text2}>10:00</Text>
+      </View>
+      <Link href={"/where"} asChild>
+        <Pressable style={styles.button} onPress={setAppointment()}>
+          <Text style={styles.buttonFont}>Bestätigen</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+};
 
-        </View>
-    );
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
+    color: "#10069F",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#10069F",
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 20,
+  },
+  buttonFont: {
+    color: "white",
+  },
+  buttonBottom: {
+    marginTop: -30,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 20,
+  },
+  text2: {
+    fontSize: 12,
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  checkbox: {
+    margin: 8,
+    color: "#111111",
+  },
+});
 
 export default Appointment;
--- app/index.js
+++ app/index.js
@@ -1,28 +1,32 @@
-import 'expo-router/entry';
-import { View, Text, Pressable, Button } from 'react-native';
-import React from 'react';
-import {Link, useRouter} from 'expo-router';
+import "expo-router/entry";
+import { View, Text, Pressable, Button } from "react-native";
+import React from "react";
+import { Link, useRouter } from "expo-router";
 
 const LoginPage = () => {
   const router = useRouter();
 
   const handlePress = () => {
-    router.replace('tabs');
+    router.replace("tabs");
   };
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
       <Text style={{ top: 0 }}>
         -------------- TENSION TERMINATOR ------------------
       </Text>
 
-          <Link href={'/QRScan'} asChild>
-              <Pressable>
-                  <Text>App Starten</Text>
-              </Pressable>
-          </Link>
-
-        {/*Verschiedene Varianten um ans Ziel zu kommen*/}
-        {/*
+      <Link href={"/QRScan"} asChild>
+        <Pressable>
+          <Text>App Starten</Text>
+        </Pressable>
+      </Link>
+      <Link href={"/appointment"} asChild>
+        <Pressable>
+          <Text>Termin planen</Text>
+        </Pressable>
+      </Link>
+      {/*Verschiedene Varianten um ans Ziel zu kommen*/}
+      {/*
         <Button title="App Starten" onPress= {() => router.push('QRscan')}>
         </Button>
 
@@ -38,7 +42,6 @@ const LoginPage = () => {
         </Link>
 
 */}
-
     </View>
   );
 };
