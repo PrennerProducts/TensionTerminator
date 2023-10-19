627422ab2c49288d514431662f7220a6fdf435e5
christine <hoepfner.christine@gmail.com>
2023-10-19 11:19:07 +0200

    updated screens (linked to stylesheet)

--- app/QRScan.jsx
+++ app/QRScan.jsx
@@ -2,29 +2,26 @@ import 'expo-router/entry';
 import { View, Text, Pressable, Button } from 'react-native';
 import React from 'react';
 import {Link, useRouter} from 'expo-router';
-import styles from './components/StyleSheet';
+import styles from "./components/StyleSheet";
 
-
-const QRcodeScreen = ({}) => {
+const QRcodeScreen = () => {
     const router = useRouter();
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>QR-Code Scannen</Text>
-
             <View style={styles.bottom}>
+            <Link href={'/tabs/barcode'} asChild>
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>Barcode Scannen</Text>
+                </Pressable>
+            </Link>
 
-                <Link href={'/tabs/barcode'} asChild>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Barcode Scannen</Text>
-                    </Pressable>
-                </Link>
-
-                <Link href={'/criteria'} asChild>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Überspringen</Text>
-                    </Pressable>
-                </Link>
+            <Link href={'/criteria'} asChild>
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>Überspringen</Text>
+                </Pressable>
+            </Link>
             </View>
         </View>
     );
