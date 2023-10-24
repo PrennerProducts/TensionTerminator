e446be23bfe105e5144585f4e0bea75d6462e7a7
Zetamasevt <92745665+Zetamasevt@users.noreply.github.com>
2023-10-24 22:31:28 +0200

    evaluationYaw and evaluationRoll united united..

--- app/tabs/EvaluationScreen.jsx
+++ app/tabs/EvaluationScreen.jsx
@@ -25,7 +25,7 @@ const EvaluationScreen = () => {
 
   const handleEvaluierung = () => {
     setEvaluationStarted(true);
-    router.replace('evaluationYaw');
+    router.replace('evaluationYR');
   };
 
   return (
