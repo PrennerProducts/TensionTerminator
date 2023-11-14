87a9a3f7a65d625c040f43fd0dc53f0b0481e90b
Zetamasevt <92745665+Zetamasevt@users.noreply.github.com>
2023-11-14 16:35:38 +0100

    HomeScreen

--- app/index.js
+++ app/index.js
@@ -2,9 +2,7 @@ import 'expo-router/entry';
 import {
   View,
   Text,
-  Pressable,
-  Button,
-  ImageBackground,
+  TouchableOpacity,
   Image,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
@@ -13,123 +11,86 @@ import styles from './components/StyleSheet';
 import UserData from './classes/userData';
 import { avatarList } from './config/avatarConfig';
 import { ProfileImageProvider } from '../app/components/ProfileImageContext';
+import { evaluationData } from './evaluationComponents/evaluationData';
 
 const LoginPage = () => {
   const router = useRouter();
-  const user = new UserData();
-  const [currentImageIndex, setCurrentImageIndex] = useState();
+  const [user, setUser] = useState(new UserData());
+  const [userName, setUserName] = useState();
+  const [selectedAvatarIndex, setselectedAvatarIndex] = useState(0);
 
- /* useEffect(() => {
+  const goToEvaluation = async () => {
+    evaluationData.resetValues();
+    evaluationData.originScreen = '../homeScreen';
+    evaluationData.isTraining = 0;
+    evaluationData.beforeAfterTraining = 0;
+    router.replace({ pathname: 'evaluationComponents/EvaluationScreen' });
+  };
+
+  useEffect(() => {
     const initializeUser = async () => {
       await user.initialize();
-      // if (user.getprofilepicture() === null) {
-      //   user.setprofilepicture(0);
-      // }
-      setCurrentImageIndex(user.getprofilepicture());
-      console.log(
-        'Tabs_layout liest aus dem speicher den Image index: ' +
-          user.getprofilepicture()
-      );
+      setUserName(user.getUserName());
+      setselectedAvatarIndex(user.getprofilepicture());
+      //console.log('Name: ', userName);
+      //console.log('Avatar Index: ', selectedAvatarIndex);
     };
-
     initializeUser();
-  });*/
-
-  const getProfileImage = () => {
-    if (
-      selectedAvatarIndex !== null &&
-      selectedAvatarIndex < avatarList.length
-    ) {
-      return avatarList[selectedAvatarIndex];
-    }
-    return require('../assets/images/avatar4.png');
-  };
-
-  // const updateImageIndex = () => {
-  //   setCurrentImageIndex(user.getprofilepicture());
-  // };
-
-  // useEffect(() => {
-  //   user.addObserver(updateImageIndex);
-
-  //   return () => {
-  //     // Dies wird beim Aushängen der Komponente aufgerufen
-  //     user.removeObserver(updateImageIndex);
-  //   };
-  // }, []);
-
-  const handlePress = () => {
-    router.replace('components/barcode');
-  };
+  }, []);
 
   return (
     <ProfileImageProvider>
-      <View style={styles.container}>
-        {/* <Image
-          source={
-            currentImageIndex >= 0 &&
-            currentImageIndex != null &&
-            currentImageIndex < avatarList.length
-              ? avatarList[currentImageIndex]
-              : require('../assets/images/error.jpg')
-          }
-          style={{
-            position: 'absolute',
-            width: '90%',
-            resizeMode: 'contain',
-            top: 20,
-          }}
-        /> */}
-        <View
-          style={{
-            position: 'absolute',
-            width: '100%',
-            flex: 1,
-            flexDirection: 'column',
-            top: 0,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Image
+      <View style={{flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  }}>
+        <View style={{
+            }}>
+          <Text style={styles.header}>Home</Text>
+          {/*<Image
             source={require('../assets/logo.png')}
             style={{
-              position: 'absolute',
               width: '100%',
               resizeMode: 'contain',
-              top: 0,
             }}
-          />
+          />*/}
           <Image
-            source={require('../assets/images/avatar1.png')}
-            style={{
-              position: 'absolute',
-              width: '30%',
-              resizeMode: 'contain',
-              top: -300,
-            }}
-          />
+          source={
+            selectedAvatarIndex >= 0 &&
+            selectedAvatarIndex != null &&
+            selectedAvatarIndex < avatarList.length
+              ? avatarList[selectedAvatarIndex]
+              : require('../assets/images/error.jpg')
+          }
+          style={{ 
+            resizeMode: 'contain',
+            width: 200, 
+            height: 200, 
+            borderRadius: 100 
+          }}
+        />
+        <Text style={styles.header}>{userName}</Text>
         </View>
         <View style={styles.bottom}>
           <Link href={'./QRScan'} asChild>
-            <Pressable style={styles.button}>
+            <TouchableOpacity style={styles.button}>
               <Text style={styles.buttonText}>QR-Code scannen</Text>
-            </Pressable>
+            </TouchableOpacity>
           </Link>
           <Link href={'./components/criteria'} asChild>
-            <Pressable style={styles.button}>
+            <TouchableOpacity style={styles.button}>
               <Text style={styles.buttonText}>Ausschlusskriterien</Text>
-            </Pressable>
+            </TouchableOpacity>
           </Link>
-          <Link href={'./evaluationComponents/EvaluationScreen'} asChild>
-            <Pressable style={styles.button}>
+
+            <TouchableOpacity style={styles.button} onPress={() => {goToEvaluation();}}>
               <Text style={styles.buttonText}>Beweglichkeitsmessung</Text>
-            </Pressable>
-          </Link>
+            </TouchableOpacity>
+
           <Link href={'./components/painWhere'} asChild>
-            <Pressable style={styles.button}>
+            <TouchableOpacity style={styles.button}>
               <Text style={styles.buttonText}>Training</Text>
-            </Pressable>
+            </TouchableOpacity>
           </Link>
         </View>
       </View>
