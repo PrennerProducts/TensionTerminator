7d4cda6052a49facf2196d5080868dbb030c4266
Lukas Prenner <prenner.products@gmail.com>
2023-11-29 22:42:33 +0100

    Video update

--- app/evaluationComponents/evaluationScreen.jsx
+++ app/evaluationComponents/evaluationScreen.jsx
@@ -6,6 +6,8 @@ import { Link, useRouter } from 'expo-router';
 import { evaluationData } from './evaluationData';
 import styles from '../components/StyleSheet';
 import { Button } from '@rneui/themed';
+import { Video, ResizeMode } from 'expo-av';
+import anleitungsvideo from '../../assets/videos/evaluation2.mp4';
 
 const evaluationScreen = () => {
   const router = useRouter();
@@ -23,6 +25,10 @@ const evaluationScreen = () => {
     router.replace('../evaluationComponents/evaluationYR');
   };
 
+  // Video
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>
@@ -30,26 +36,24 @@ const evaluationScreen = () => {
       </Text>
       <ScrollView style={{ flex: 1 }}>
         <Text style={styles.text}>
-            Die Evaluierung besteht aus 2 Übungen. {'\n'}
-            Mit diesen Übungen wird die Beweglichkeit des Kopfes evaluiert. {'\n'}
+          Die Evaluierung besteht aus 2 Übungen. {'\n'}
+          Mit diesen Übungen wird die Beweglichkeit des Kopfes evaluiert. {'\n'}
         </Text>
-          <Text style={styles.textUnderline}>
-            Erste Übung Yaw (Kopfrotation)
-          </Text>
-            <Text style={styles.text}>
-            Dafür den Kopf zuerst nach links oder rechts drehen, kurz am Maximum halten
-            und dann wieder in die Ausgangsposition zurückdrehen. {'\n'}
-            Dann den Kopf in die andere Richtung drehen, kurz am Maximum halten und wieder
-            in die Ausgangsposition zurückdrehen. {'\n'}
-            </Text>
-            <Text style={styles.textUnderline}>
-            Zweite Übung Rol (Kopf zu Schulterbewegung)
-            </Text>
-            <Text style={styles.text}>
-            Dafür den Kopf zuerst nach links oder rechts neigen, kurz am Maximum halten
-            und dann wieder in die Ausgangsposition zurückneigen. {'\n'}
-            Dann den Kopf in die andere Richtung neigen, kurz am Maximum halten und wieder
-            in die Ausgangsposition zurückneigen. {'\n'}
+        <Text style={styles.textUnderline}>Erste Übung Yaw (Kopfrotation)</Text>
+        <Text style={styles.text}>
+          Dafür den Kopf zuerst nach links oder rechts drehen, kurz am Maximum
+          halten und dann wieder in die Ausgangsposition zurückdrehen. {'\n'}
+          Dann den Kopf in die andere Richtung drehen, kurz am Maximum halten
+          und wieder in die Ausgangsposition zurückdrehen. {'\n'}
+        </Text>
+        <Text style={styles.textUnderline}>
+          Zweite Übung Rol (Kopf zu Schulterbewegung)
+        </Text>
+        <Text style={styles.text}>
+          Dafür den Kopf zuerst nach links oder rechts neigen, kurz am Maximum
+          halten und dann wieder in die Ausgangsposition zurückneigen. {'\n'}
+          Dann den Kopf in die andere Richtung neigen, kurz am Maximum halten
+          und wieder in die Ausgangsposition zurückneigen. {'\n'}
         </Text>
 
         <View
@@ -69,7 +73,7 @@ const evaluationScreen = () => {
             style={{ width: 100, height: 100 }}
           />
         </View>
-          <Text>{'\n'}</Text>
+        <Text>{'\n'}</Text>
       </ScrollView>
 
       <View style={styles.bottom}>
@@ -85,14 +89,35 @@ const evaluationScreen = () => {
         >
           <View style={{ flex: 1 }}>
             {/*Vimeo Videos können nicht direkt mit react-native-video wiedergegeben werden deswegen WebView*/}
-            <WebView
+            {/* <WebView
               source={{
                 uri: 'https://player.vimeo.com/video/853027148?autoplay=1',
               }}
               mediaPlaybackRequiresUserAction={false}
               style={{ flex: 1 }}
-            />
+            /> */}
 
+            <Video
+              ref={video}
+              style={{ flex: 1 }}
+              source={anleitungsvideo}
+              useNativeControls
+              resizeMode={ResizeMode.COVER}
+              isLooping
+              shouldPlay
+              onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+              // onReadyForDisplay={() => video.current.presentFullscreenPlayer()}
+            />
+            {/* <View style={styles.button}>
+              <Button
+                title={status.isPlaying ? 'Pause' : 'Play'}
+                onPress={() =>
+                  status.isPlaying
+                    ? video.current.pauseAsync()
+                    : video.current.playAsync()
+                }
+              />
+            </View> */}
             <View style={{ alignItems: 'center' }}>
               <Button
                 title="Schließen"
